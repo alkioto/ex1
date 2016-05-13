2230966421d143cff03458efcd7ed1c2bd74337a
alkioto <alkioto@gmail.com>
2016-05-13 15:58:21 +0300

    1.0

---
+++ 1.py
@@ -0,0 +1,14 @@
+print("Hello world !")
+
+def xxx():
+    def f1():
+        ''' Привет! ЁЁЁ """" '''
+        pass
+    def f2():
+        print("WWW")
+        pass
+    return f1,f2
+        
+a,b = xxx()
+a.__doc__
+b()
